34e87acf1d40c18601aa0232d97d915be815e859
butackle <butackle@gmail.com>
2016-03-15 01:25:38 +0000

    グループUsersテストを作成

--- Test/Case/Controller/GroupsController/UsersTest.php
+++ Test/Case/Controller/GroupsController/UsersTest.php
@@ -9,7 +9,7 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
-App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+App::uses('GroupsControllerTestCase', 'Groups.Test/Case/Controller');
 
 /**
  * GroupsController::users()のテスト
@@ -17,55 +17,7 @@ App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
  * @author Yuna Miyashita <butackle@gmail.com>
  * @package NetCommons\Groups\Test\Case\Controller\GroupsController
  */
-class GroupsControllerUsersTest extends NetCommonsControllerTestCase {
-
-/**
- * Fixtures
- *
- * @var array
- */
-	public $fixtures = array(
-		'plugin.groups.group',
-		'plugin.groups.groups_user',
-	);
-
-/**
- * Plugin name
- *
- * @var string
- */
-	public $plugin = 'groups';
-
-/**
- * Controller name
- *
- * @var string
- */
-	protected $_controller = 'groups';
-
-/**
- * setUp method
- *
- * @return void
- */
-	public function setUp() {
-		parent::setUp();
-
-		//ログイン
-		TestAuthGeneral::login($this);
-	}
-
-/**
- * tearDown method
- *
- * @return void
- */
-	public function tearDown() {
-		//ログアウト
-		TestAuthGeneral::logout($this);
-
-		parent::tearDown();
-	}
+class GroupsControllerUsersTest extends GroupsControllerTestCase {
 
 /**
  * users()アクションのGetリクエストテスト
@@ -74,11 +26,12 @@ class GroupsControllerUsersTest extends NetCommonsControllerTestCase {
  */
 	public function testUsersGet() {
 		//テスト実行
-		$this->_testGetAction(array('action' => 'users'), array('method' => 'assertNotEmpty'), null, 'view');
-
-		//チェック
-		//MUST:assert追加
-		debug($this->view);
+		$this->_testGetAction(
+			array('action' => 'users'),
+			array('method' => 'assertNotEmpty'),
+			null,
+			'view'
+		);
 	}
 
 }
