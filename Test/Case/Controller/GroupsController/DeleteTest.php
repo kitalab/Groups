bf136ecbdf6a7e499aedf91fcc4793c7e3460b1c
butackle <butackle@gmail.com>
2016-03-15 01:20:44 +0000

    グループ削除テストを作成

--- Test/Case/Controller/GroupsController/DeleteTest.php
+++ Test/Case/Controller/GroupsController/DeleteTest.php
@@ -9,7 +9,7 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
-App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+App::uses('GroupsControllerTestCase', 'Groups.Test/Case/Controller');
 
 /**
  * GroupsController::delete()のテスト
@@ -17,68 +17,107 @@ App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
  * @author Yuna Miyashita <butackle@gmail.com>
  * @package NetCommons\Groups\Test\Case\Controller\GroupsController
  */
-class GroupsControllerDeleteTest extends NetCommonsControllerTestCase {
-
-/**
- * Fixtures
- *
- * @var array
- */
-	public $fixtures = array(
-		'plugin.groups.group',
-		'plugin.groups.groups_user',
-	);
-
-/**
- * Plugin name
- *
- * @var string
- */
-	public $plugin = 'groups';
-
-/**
- * Controller name
- *
- * @var string
- */
-	protected $_controller = 'groups';
-
-/**
- * setUp method
- *
- * @return void
- */
-	public function setUp() {
-		parent::setUp();
-
-		//ログイン
-		TestAuthGeneral::login($this);
-	}
-
-/**
- * tearDown method
- *
- * @return void
- */
-	public function tearDown() {
-		//ログアウト
-		TestAuthGeneral::logout($this);
-
-		parent::tearDown();
-	}
+class GroupsControllerDeleteTest extends GroupsControllerTestCase {
 
 /**
  * delete()アクションのGetリクエストテスト
- *
+ * 
+ * @dataProvider dataProviderParamId
+ * @param $id ID
+ * @param $exception	想定されるエラー
  * @return void
  */
-	public function testDeleteGet() {
-		//テスト実行
-		$this->_testGetAction(array('action' => 'delete'), array('method' => 'assertNotEmpty'), null, 'view');
+	public function testDeleteGet($id, $exception) {
+		$this->__testNotAllowDelete('get', $id, $exception);
+	}
 
-		//チェック
-		//MUST:assert追加
-		debug($this->view);
+/**
+ * delete()アクションのPutリクエストテスト
+ * 
+ * @dataProvider dataProviderParamId
+ * @param $id ID
+ * @param $exception	想定されるエラー
+ * @return void
+ */
+	public function testDeletePut($id, $exception) {
+		$this->__testNotAllowDelete('put', $id, $exception);
+	}
+
+/**
+ * delete()アクションのPostリクエストテスト
+ * 
+ * @dataProvider dataProviderParamId
+ * @param $id ID
+ * @param $exception	想定されるエラー
+ * @return void
+ */
+	public function testDeletePost($id, $exception) {
+		$this->__testAllowDelete('post', $id, $exception);
+	}
+
+/**
+ * delete()アクションのDeleteリクエストテスト
+ * 
+ * @dataProvider dataProviderParamId
+ * @param $id ID
+ * @param $exception	想定されるエラー
+ * @return void
+ */
+	public function testDeleteDelete($id, $exception) {
+		$this->__testAllowDelete('delete', $id, $exception);
+	}
+
+/**
+ * delete()アクションの許可されているRESTのテスト
+ * 
+ * @param $rest REST
+ * @param $id ID
+ * @param $exception	想定されるエラー
+ * @return void
+ */
+	private function __testAllowDelete($rest, $id, $exception) {
+		$this->__testDeleteAction($rest, $id, $exception);
+		//データが削除されているかを確認
+		$this->assertCount(0, $this->__group->find('all'));
+	}
+
+/**
+ * delete()アクションの許可されていないRESTのテスト
+ * 
+ * @param $rest REST
+ * @param $id ID
+ * @param $exception	想定されるエラー
+ * @return void
+ */
+	private function __testNotAllowDelete($rest, $id, $exception) {
+		//存在するIDが入力されていた場合にはonlyAllowでエラーが出る
+		if (is_null($exception)) {
+			$exception = "MethodNotAllowedException";
+		}
+
+		$this->__testDeleteAction($rest, $id, $exception);
+	}
+
+/**
+ * deleteテスト実行
+ * 
+ * @param $rest REST
+ * @param $id ID
+ * @param $exception	想定されるエラー
+ * @return void
+ */
+	private function __testDeleteAction($rest, $id, $exception) {
+		$this->_testNcAction(
+			array(
+				'plugin' => $this->plugin,
+				'controller' => $this->_controller,
+				'action' => 'delete',
+				$id
+			),
+			['method' => $rest],
+			$exception,
+			'view'
+		);
 	}
 
 }
