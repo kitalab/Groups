46e968cb8aec51e39a3c465be9802587c2609be0
butackle <butackle@gmail.com>
2016-03-17 06:18:52 +0000

    コンポーネントのテストを作成

--- Test/Case/Controller/Component/GroupsComponent/SetGroupListTest.php
+++ Test/Case/Controller/Component/GroupsComponent/SetGroupListTest.php
@@ -9,7 +9,7 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
-App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+App::uses('GroupsControllerTestCase', 'Groups.Test/Case/Controller/GroupsController');
 
 /**
  * GroupsComponent::setGroupList()のテスト
@@ -17,72 +17,156 @@ App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
  * @author Yuna Miyashita <butackle@gmail.com>
  * @package NetCommons\Groups\Test\Case\Controller\Component\GroupsComponent
  */
-class GroupsComponentSetGroupListTest extends NetCommonsControllerTestCase {
+class GroupsComponentSetGroupListTest extends GroupsControllerTestCase {
 
 /**
- * Fixtures
+ * UploadFileモデル名
+ * 
+ * @var string
+ */
+	private $__modelUploadFile = 'UploadFile';
+
+/**
+ * Groupモデル名
+ * 
+ * @var string
+ */
+	private $__modelGroup = 'Group';
+
+/**
+ * Fixtures Setting
  *
  * @var array
  */
-	public $fixtures = array();
+	public function __construct($name = null, array $data = array(), $dataName = '') {
+		$this->fixtures = array_merge(
+			$this->fixtures,
+			[
+				'plugin.groups.group4_users_test',
+				'plugin.groups.groups_user4_users_test'
+			]
+		);
 
-/**
- * Plugin name
- *
- * @var string
- */
-	public $plugin = 'groups';
-
-/**
- * setUp method
- *
- * @return void
- */
-	public function setUp() {
-		parent::setUp();
-
-		//テストプラグインのロード
-		NetCommonsCakeTestCase::loadTestPlugin($this, 'Groups', 'TestGroups');
-	}
-
-/**
- * tearDown method
- *
- * @return void
- */
-	public function tearDown() {
-		//ログアウト
-		TestAuthGeneral::logout($this);
-
-		parent::tearDown();
+		parent::__construct($name, $data, $dataName);
 	}
 
 /**
  * setGroupList()のテスト
  *
+ * @dataProvider dataProviderSetGroupList
+ * @param string $strGroupId
  * @return void
  */
-	public function testSetGroupList() {
-		//テストコントローラ生成
-		$this->generateNc('TestGroups.TestGroupsComponent');
-
-		//ログイン
-		TestAuthGeneral::login($this);
-
-		//テストアクション実行
-		$this->_testGetAction('/test_groups/test_groups_component/index',
-				array('method' => 'assertNotEmpty'), null, 'view');
-		$pattern = '/' . preg_quote('Controller/Component/TestGroupsComponent', '/') . '/';
-		$this->assertRegExp($pattern, $this->view);
-
-		//テストデータ生成
-		$query = array();
+	public function testSetGroupList($strGroupId = null) {
+		$groupIds = explode(',', $strGroupId);
+		$userIds = $this->_getExpectedUserIds([$strGroupId]);
 
 		//テスト実行
-		$this->controller->Groups->setGroupList($query, $this->controller);
-
-		//SHOULD:必要に応じてassert追加する
-		debug($this->controller->viewVars);
+		Current::initialize($this->controller->request);
+		$this->controller->Groups->setGroupList(
+			$this->controller,
+			$this->__createGroupCondition($groupIds)
+		);
+		//セットしたviewVarsに関する変数を設定
+		$viewValueKeys = array(
+			'users' => ['User', ['User', $this->__modelUploadFile ], $userIds],
+			'groups' => ['Group', [$this->__modelGroup, 'GroupsUser'], $groupIds]
+		);
+		$viewValues = $this->controller->viewVars;
+		//データ検証(全体)
+		$this->assertCount(count($viewValueKeys), $viewValues);
+		foreach ($viewValueKeys as $viewValueKey => $checkOption) {
+			$this->assertArrayHasKey($viewValueKey, $viewValues);
+			//データ検証（データ内容）
+			if (empty($userIds)) {
+				$this->assertEquals($viewValues[$viewValueKey], array());
+			} else {
+				$this->__validateViewValues($viewValues[$viewValueKey], $checkOption);
+			}
+		}
 	}
 
+/**
+ * testSetGroupList用dataProvider
+ * 
+ * ### 戻り値
+ *  - strGroupId : データ検索するグループID
+ */
+	public function dataProviderSetGroupList() {
+		return array(
+			['1'], ['2'], ['3'], ['1,2'], ['2,1'],
+		);
+	}
+
+/**
+ * Groupモデルでの取得クエリに渡す条件配列を作成
+ * 
+ * @param array $groupIds
+ * @return array
+ */
+	private function __createGroupCondition($groupIds) {
+		$orArray = array();
+		foreach ($groupIds as $groupId) {
+			$orArray[] = ['Group.id' => $groupId];
+		}
+
+		return array(
+			'conditions' => [
+				'or' => $orArray
+			]
+		);
+	}
+
+/**
+ * セットした値が正しいかを確認
+ * 
+ * @var array $actualDataArray　セットした値
+ * @var array $checkOption 検索情報
+ * @return void
+ */
+	private function __validateViewValues($actualDataArray, $checkOption) {
+		$modelName = $checkOption[0];
+		$checkKeys = $checkOption[1];
+		$expectedIds = $checkOption[2];
+		//データ数確認
+		$this->assertCount(
+			count($expectedIds),
+			$actualDataArray
+		);
+		//データ詳細確認
+		sort($expectedIds);
+		foreach ($expectedIds as $index => $expectedId) {
+			$actualData = $actualDataArray[$index];
+			//データ項目数確認
+			$this->assertCount(
+				count($checkKeys),
+				$actualData
+			);
+			//データ内容確認
+			$dbData = $this->controller->$modelName->findById($expectedId);
+			foreach ($checkKeys as $checkKey) {
+				if ($checkKey === $this->__modelUploadFile) {
+					if (!isset($dbData[$checkKey])) {
+						continue;
+					}
+					$expectedData = array_merge(
+						$dbData[$checkKey]['avatar'],
+						$dbData[$checkKey]
+					);
+				} else {
+					$expectedData = $dbData[$checkKey];
+				}
+				if ($checkKey === $this->__modelGroup) {
+					unset($expectedData['created_user']);
+					unset($expectedData['created']);
+					unset($expectedData['modified_user']);
+				}
+
+				$this->assertEquals(
+					$expectedData,
+					$actualData[$checkKey]
+				);
+			}
+		}
+	}
 }
